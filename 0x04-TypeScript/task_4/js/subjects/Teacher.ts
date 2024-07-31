7835f72b4c44cf61046d5e495bb57e5d27e8e919
Felix Opuge <omondifelix702@gmail.com>
2024-07-31 19:58:51 +0300

    Namespace & Declaration merging

---
+++ 0x04-TypeScript/task_4/js/subjects/Teacher.ts
@@ -0,0 +1,8 @@
+//Defining the Subject namespace
+namespace Subjects {
+    //Defining the Teacher Interface within the Subjects namespace
+    export interface Teacher {
+        firstName: string;
+        lastName: string;
+    }
+}
